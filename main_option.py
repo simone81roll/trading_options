ca9dd1ff6aa755f0cf2b8ab34ee36e7a294856dc
simone81roll <simone_spiritelli@tiscali.it>
2026-05-30 22:36:24 +0200

    Update main_option.py

--- main_option.py
+++ main_option.py
@@ -5,6 +5,7 @@ pages = {
         st.Page("option_app.py", title="Versione 1", icon=":material/counter_1:"),
         st.Page("option_app_v2.py", title="Versione 2", icon=":material/counter_2:"),
         st.Page("option_app_v3.py", title="Versione 3", icon=":material/counter_3:"),
+        st.Page("masaniello.py", title="Masaniello", icon=":material/counter_4:"),
     ],
 }
 
