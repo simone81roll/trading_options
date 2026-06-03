003d73adf87dedf2d7da2169f27aec931d942e09
simone81roll <simone_spiritelli@tiscali.it>
2026-06-03 23:45:05 +0200

    Update main_option.py

--- main_option.py
+++ main_option.py
@@ -6,7 +6,7 @@ pages = {
         st.Page("option_app_v2.py", title="Versione 2", icon=":material/counter_2:"),
         st.Page("option_app_v3.py", title="Versione 3", icon=":material/counter_3:"),
         st.Page("option_app_v4_ai.py", title="Versione AI Lab"),
-        st.Page("options_edge_finder.py", title="Option EDGE", icon=":material/counter_5:"),
+        st.Page("options_edge_finder.py", title="Option EDGE"),
         st.Page("masaniello.py", title="Masaniello quota FISSA", icon=":material/counter_4:"),
     ],
 }
