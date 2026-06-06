ce757a17ec4ccacdaeb5a02f021f29ac8ce6685b
simone81roll <simone_spiritelli@tiscali.it>
2026-06-06 16:32:25 +0200

    Update main_option.py

--- main_option.py
+++ main_option.py
@@ -8,7 +8,7 @@ pages = {
         st.Page("option_edge_csv.py", title="Option EDGE CSV"),
         st.Page("option_app_v4_ai.py", title="Versione AI Lab"),
         st.Page("option_edge_finder.py", title="Option EDGE"),
-        st.Page("masaniello.py", title="Masaniello quota FISSA", icon=":material/counter_4:"),
+        st.Page("EDGE_SMA_200.py", title="EDGE SMA200", icon=":material/counter_4:"),
     ],
 }
 
